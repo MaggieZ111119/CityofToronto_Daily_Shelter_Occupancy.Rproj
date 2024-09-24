8302a94b69069b1fca41175ed144e20e3665d7bf
MaggieZ111119 <maggiey.zhang@mail.utoronto.ca>
2024-09-24 14:03:05 -0400

    download data(2020,2019,2018,2017)

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,21 +11,45 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
+library(dprly)
 
 #### Download data ####
 
+### Shelter occupancy 2020 ###
+#Downloaded data for Shelter occupancy in the year 2020 from OpenDataToronto as csv. file
 
-raw_marriage_data <-
-  read_csv(
-    file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/e28bc818-43d5-43f7-b5d9-bdfb4eda5feb/resource/01dff98a-b56b-4237-bb5d-f56319f0ced6/download/Marriage%20Licence%20Statistics%20Data.csv",
-    show_col_types = FALSE
-  )
+raw_2020_shelter <- read_csv(file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/8a6eceb2-821b-4961-a29d-758f3087732d/resource/9e076fe4-2f86-48d7-a6e4-93710ca715ae/download/Daily%20shelter%20occupancy%202020.csv",
+                             show_col_types = FALSE)
+#Save Data
+write_csv(raw_2020_shelter, 
+          "data/raw_data/raw_2020_shelter_data.csv") 
 
 
+### Shelter occupancy 2019 ###
+#Downloaded data for Shelter occupancy in the year 2019 from OpenDataToronto as csv. file
 
-#### Save data ####
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(raw_marriage_data, 
-          "starter_folder-main/data/raw_data/raw_marriage_data.csv") 
+raw_2019_shelter <- read_csv(file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/8a6eceb2-821b-4961-a29d-758f3087732d/resource/3ea9d9ba-f05f-4055-bcc3-333f699c5e77/download/Daily%20shelter%20occupancy%202019.csv",
+                             show_col_types = FALSE)
+#Save Data
+write_csv(raw_2019_shelter, 
+          "data/raw_data/raw_2019_shelter_data.csv") 
 
-         
+
+### Shelter occupancy 2018 ###
+#Downloaded data for Shelter occupancy in the year 2018 from OpenDataToronto as csv. file
+
+raw_2018_shelter <- read_csv(file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/8a6eceb2-821b-4961-a29d-758f3087732d/resource/af47a4d4-4339-446d-bf53-7983f78a80bc/download/Daily%20shelter%20occupancy%202018.csv",
+                             show_col_types = FALSE)
+#Save Data
+write_csv(raw_2018_shelter, 
+          "data/raw_data/raw_2018_shelter_data.csv") 
+
+
+### Shelter occupancy 2017 ###
+#Downloaded data for Shelter occupancy in the year 2017 from OpenDataToronto as csv. file
+
+raw_2017_shelter <- read_csv(file = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/8a6eceb2-821b-4961-a29d-758f3087732d/resource/c07041d2-633f-43cf-8372-8133474bf4f4/download/Daily%20shelter%20occupancy%202017.csv",
+                             show_col_types = FALSE)
+#Save Data
+write_csv(raw_2017_shelter, 
+          "data/raw_data/raw_2017_shelter_data.csv") 
