99a69abd2b2c6db491fa748b8b22767eadb5a9ae
MaggieZ111119 <maggiey.zhang@mail.utoronto.ca>
2024-09-24 15:57:46 -0400

    Data Cleaning (select intrested columns, creating columns contains useful information, and fixing format)

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,10 +9,64 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
 
 #### Clean data ####
-raw_marriage_data <- read_csv(
-  "starter_folder-main/data/raw_data/raw_marriage_data.csv")
+raw_2017_shelter <- data.frame(read_csv(
+  "data/raw_data/raw_2017_shelter_data.csv", show_col_types = FALSE))
+raw_2018_shelter <- data.frame(read_csv(
+  "data/raw_data/raw_2018_shelter_data.csv", show_col_types = FALSE))
+raw_2019_shelter <- data.frame(read_csv(
+  "data/raw_data/raw_2019_shelter_data.csv", show_col_types = FALSE))
+raw_2020_shelter <- data.frame(read_csv(
+  "data/raw_data/raw_2020_shelter_data.csv", show_col_types = FALSE))
+
+
+## Fix issue with data in four dataset, and select columns within our interest.
+#2017 Data
+cleaned_2017 <- raw_2017_shelter %>%
+  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) %>% 
+  summarize(
+    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
+    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    .groups = 'drop') %>%
+  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
+         Totoal_Occupancy, Totoal_Capacity)
+
+#2018 Data
+cleaned_2018 <- raw_2018_shelter %>%
+  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) %>% 
+  summarize(
+    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
+    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    .groups = 'drop') %>%
+  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
+         Totoal_Occupancy, Totoal_Capacity)
+
+#2019 Data
+cleaned_2019 <- raw_2019_shelter %>%
+  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) %>% 
+  summarize(
+    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
+    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    .groups = 'drop') %>%
+  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
+         Totoal_Occupancy, Totoal_Capacity)
+
+#2020 Data
+cleaned_2020 <- raw_2020_shelter %>%
+  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) %>% 
+  summarize(
+    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
+    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    .groups = 'drop') %>%
+  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
+         Totoal_Occupancy, Totoal_Capacity)
+
+## Combine 4 dataset in order
+full_shelter_data <- rbind(raw_2017_shelter, raw_2018_shelter,
+                  raw_2019_shelter, raw_2020_shelter)
+
 
 cleaned_data <-
   raw_marriage_data |>
