cdac9366f2bd46f321ee148c8276199a16bbd67c
MaggieZ111119 <maggiey.zhang@mail.utoronto.ca>
2024-09-26 11:52:32 -0400

    Move test code for simulated data to test script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -81,28 +81,5 @@ for (date in Date_seq) {
 # head(simulated_dataset)
 
 
-## TEST1: Missing Value
-num_missing_values <- sum(is.na(simulated_dataset))
-cat("Total missing values: ", num_missing_values, "\n")
-
-
-## TEST2: Negative value (Total_Occupancy and Total_Capacity)
-negative_occupancy <- sum(simulated_dataset$Total_Occupancy < 0)
-negative_capacity <- sum(simulated_dataset$Total_Capacity < 0)
-cat("Total negative occupancy values: ", negative_occupancy, "\n")
-cat("Total negative capacity values: ", negative_capacity, "\n")
-
-
-## TEST3: Date Format is consistent
-incorrect_dates <- sum(!grepl("^\\d{4}-\\d{2}-\\d{2}$", simulated_dataset$OCCUPANCY_DATE))
-
-if (incorrect_dates > 0) {
-  message("Warning: ", incorrect_dates, " dates are not in the correct format (YYYY-MM-DD).")
-} else {
-  message("All dates are in the correct format (YYYY-MM-DD).")
-}
-
-
-
 # Write and store the simulated dataset
 write_csv(simulated_dataset, file = "data/raw_data/simulated_dataset_shelter_data.csv")
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Test and check of the simulated data
 # Author: Maggie Zhang
-# Date: 19 September 2024
+# Date: 26 September 2024
 # Contact: maggiey.zhang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: have the data ready into R
@@ -13,11 +13,27 @@ library(tidyverse)
 
 
 #### Test data ####
-data <- read.csv("starter_folder-main/data/raw_data/simulated.csv")
-
-# Test for Neg
-data$number_of_marriage |> min() <= 0
+simulated_dataset <- read.csv("data/raw_data/simulated_dataset_shelter_data.csv")
 
 
-# Test for NAs
-all(is.na(data$number_of_marriage))
+## TEST1: Missing Value
+num_missing_values <- sum(is.na(simulated_dataset))
+cat("Total missing values: ", num_missing_values, "\n")
+
+
+## TEST2: Negative value (Total_Occupancy and Total_Capacity)
+negative_occupancy <- sum(simulated_dataset$Total_Occupancy < 0)
+negative_capacity <- sum(simulated_dataset$Total_Capacity < 0)
+cat("Total negative occupancy values: ", negative_occupancy, "\n")
+cat("Total negative capacity values: ", negative_capacity, "\n")
+
+
+## TEST3: Date Format is consistent
+incorrect_dates <- sum(!grepl("^\\d{4}-\\d{2}-\\d{2}$", simulated_dataset$OCCUPANCY_DATE))
+
+if (incorrect_dates > 0) {
+  message("Warning: ", incorrect_dates, " dates are not in the correct format (YYYY-MM-DD).")
+} else {
+  message("All dates are in the correct format (YYYY-MM-DD).")
+}
+
