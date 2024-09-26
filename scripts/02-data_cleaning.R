6aed737e5b16e43c10bcb31917632ba07a503fed
MaggieZ111119 <maggiey.zhang@mail.utoronto.ca>
2024-09-26 12:22:30 -0400

    Clean names of columns, make them consistent

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -25,55 +25,97 @@ raw_2020_shelter <- data.frame(read_csv(
 ## Fix issue with data in four dataset, and select columns within our interest.
 #2017 Data
 cleaned_2017 <- raw_2017_shelter |>
-  mutate(date = as.Date(raw_2017_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d")) |>
-  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) |> 
+  # clean names of columns 
+  janitor::clean_names() |>
+  # change date to recognizable format
+  mutate(date = as.Date(raw_2017_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d"))
+
+cleaned_2017 <- cleaned_2017 |>
+  # get information needed for analysis with my interest
+  group_by(occupancy_date,shelter_city,sector) |> 
   summarize(
-    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
-    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_occupancy = sum(occupancy, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_capacity = sum(capacity, na.rm = TRUE),
     .groups = 'drop') |>
-  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
-         Totoal_Occupancy, Totoal_Capacity)
-cleaned_2017$OCCUPANCY_DATE <-  as.Date(cleaned_2017$OCCUPANCY_DATE, 
+  select(occupancy_date, shelter_city, sector, 
+         totoal_occupancy, totoal_capacity)
+
+# Make sure date format is consistent with other three datasets
+cleaned_2017$occupancy_date <-  as.Date(cleaned_2017$occupancy_date, 
                                         as.is = double, na.rm = TRUE)
 
 
 #2018 Data
 cleaned_2018 <- raw_2018_shelter |>
-  mutate(date = as.Date(raw_2018_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d")) |>
-  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) |> 
+  # clean names of columns 
+  janitor::clean_names() |>
+  # change date to recognizable format
+  mutate(date = as.Date(raw_2018_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d"))
+
+cleaned_2018 <- cleaned_2018 |>
+  # get information needed for analysis with my interest
+  group_by(occupancy_date,shelter_city,sector) |> 
   summarize(
-    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
-    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_occupancy = sum(occupancy, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_capacity = sum(capacity, na.rm = TRUE),
     .groups = 'drop') |>
-  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
-         Totoal_Occupancy, Totoal_Capacity)
-cleaned_2018$OCCUPANCY_DATE <-  as.Date(cleaned_2018$OCCUPANCY_DATE, 
+  select(occupancy_date, shelter_city, sector, 
+         totoal_occupancy, totoal_capacity)
+
+# Make sure date format is consistent with other three datasets
+cleaned_2018$occupancy_date <-  as.Date(cleaned_2018$occupancy_date, 
                                         as.is = double, na.rm = TRUE)
 
 #2019 Data
 cleaned_2019 <- raw_2019_shelter |>
-  mutate(date = as.Date(raw_2019_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d")) |>
-  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) |> 
+  # clean names of columns 
+  janitor::clean_names() |>
+  # change date to recognizable format
+  mutate(date = as.Date(raw_2019_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d"))
+
+cleaned_2019 <- cleaned_2019 |>
+  # get information needed for analysis with my interest
+  group_by(occupancy_date,shelter_city,sector) |> 
   summarize(
-    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
-    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_occupancy = sum(occupancy, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_capacity = sum(capacity, na.rm = TRUE),
     .groups = 'drop') |>
-  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
-         Totoal_Occupancy, Totoal_Capacity)
-cleaned_2019$OCCUPANCY_DATE <-  as.Date(cleaned_2019$OCCUPANCY_DATE, 
+  select(occupancy_date, shelter_city, sector, 
+         totoal_occupancy, totoal_capacity)
+
+# Make sure date format is consistent with other three datasets
+cleaned_2019$occupancy_date <-  as.Date(cleaned_2019$occupancy_date, 
                                         as.is = double, na.rm = TRUE)
 
 #2020 Data
 cleaned_2020 <- raw_2020_shelter |>
-  group_by(OCCUPANCY_DATE,SHELTER_CITY,SECTOR) |> 
-  summarize(
-    Totoal_Occupancy = sum(OCCUPANCY, na.rm = TRUE),
-    Totoal_Capacity = sum(CAPACITY, na.rm = TRUE),
-    .groups = 'drop') |>
-  select(OCCUPANCY_DATE, SHELTER_CITY, SECTOR, 
-         Totoal_Occupancy, Totoal_Capacity)
+  # clean names of columns 
+  janitor::clean_names() |>
+  # change date to recognizable format
+  mutate(date = as.Date(raw_2020_shelter$OCCUPANCY_DATE, format = "%Y-%m-%d"))
 
-cleaned_2020$OCCUPANCY_DATE <- format(as.Date(cleaned_2020$OCCUPANCY_DATE, format = "%m/%d/%Y"), "%Y-%m-%d")
+cleaned_2020 <- cleaned_2020 |>
+  # get information needed for analysis with my interest
+  group_by(occupancy_date,shelter_city,sector) |> 
+  summarize(
+    # occupancy number in each city for each sector 
+    totoal_occupancy = sum(occupancy, na.rm = TRUE),
+    # occupancy number in each city for each sector 
+    totoal_capacity = sum(capacity, na.rm = TRUE),
+    .groups = 'drop') |>
+  select(occupancy_date, shelter_city, sector, 
+         totoal_occupancy, totoal_capacity)
+
+# Make sure date format is consistent with other three datasets
+# Special notice here, 2020 have different date naming structure tha others.
+cleaned_2020$occupancy_date <- format(as.Date(cleaned_2020$occupancy_date, 
+                                              format = "%m/%d/%Y"), "%Y-%m-%d")
 
 
 ## Combine 4 dataset in order
